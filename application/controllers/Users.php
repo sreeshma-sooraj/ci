a63f4a8e01d54c4d869cc62c3a4580f4aa998786
sreeshmasooraj <sreesma93@gmail.com>
2023-03-27 13:54:04 +0530

    lesson11:Processing a result from a model in a controller

--- application/controllers/Users.php
+++ application/controllers/Users.php
@@ -5,6 +5,9 @@ public function show(){
 
    // $this->load->model('user_model');
     $result = $this->user_model->get_users();
+    foreach ($result as $object){
+        echo $object->username . "<br>";
+    }
 
 }
 }
