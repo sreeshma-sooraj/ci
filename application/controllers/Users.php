550ef2ef38f41d8876292568958106dce4b91dc6
sreeshmasooraj <sreesma93@gmail.com>
2023-03-27 13:42:15 +0530

    lesson10: Auto-loading Model

--- application/controllers/Users.php
+++ application/controllers/Users.php
@@ -3,7 +3,7 @@ class Users extends CI_Controller{
 public function show(){
 
 
-    $this->load->model('user_model');
+   // $this->load->model('user_model');
     $result = $this->user_model->get_users();
 
 }
--- application/models/User_model.php
+++ application/models/User_model.php
@@ -1,7 +1,24 @@
 <?php
 class User_model extends CI_Model{
     public function get_users(){
-        
+
+        $query = $this->db->get('users');
+        return $query->result();
+       
+       // $config['hostname'] = "localhost";
+      //  $config['username'] = "root";
+      //  $config['password'] = "";
+       // $config['database'] = "errand_db";
+
+     
+
+       // $config_2['hostname'] = "localhost";
+       // $config_2['username'] = "root_2";
+       // $config_2['password'] = "";
+       // $config_2['database'] = "errand_db_2";
+
+       // $connection = $this->load->database($config);
+       // $connection_2 = $this->load->database($config_2);
     }
 }
 ?>
