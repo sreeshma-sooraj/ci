e7184855c9a1abe83f34e2a620d0fdc5d1c4dfd2
sreeshmasooraj <sreesma93@gmail.com>
2023-03-28 13:19:54 +0530

    lesson23: Creating main file structure and loading main content

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -2,8 +2,12 @@
 class Home extends CI_Controller{
     public function index()
     {
+
+        $data['main_view'] = "home_view";
+        $this->load->view('layouts/main', $data);
+
        // echo" my first programe ";
-       $this->load->view('home_view');
+       //$this->load->view('home_view');
     }
 }
 ?>
--- application/views/layouts/main.php
+++ application/views/layouts/main.php
@@ -9,6 +9,20 @@
     <script src="<?php echo base_url(); ?>assets/js/bootstrap.min.js"></script>
 </head>
 <body>
+
+<div class="container">
+
+
+<div class="col-xs-3">
+
+
+</div>
+<div class="col-xs-9">
+
+
+<?php $this->load->view($main_view);?>
+</div>
+</div>
     
 </body>
 </html>
