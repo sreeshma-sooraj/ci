1d49019edad04b08db3e4eab8fb616f96c308772
sreeshmasooraj <sreesma93@gmail.com>
2023-03-28 16:10:44 +0530

    lesson26: Creating the username input and attributes in the CodeIgniter way

--- application/views/users/login_view.php
+++ application/views/users/login_view.php
@@ -1,4 +1,20 @@
 <h4>Login Form</h4>
 <?php $attribute = array('id' => 'login_form' , 'class' => 'form_horizontal'); ?>
 <?php echo form_open('users/login_view',$attribute);?>
+
+<div class="form-group">
+
+<?php echo form_label('Username');?>
+
+
+<?php 
+
+    $data = array(
+    'class' => 'form_control',
+    'name' => 'username',
+    'placeholder' => 'Enter username'
+    );
+?>
+<?php echo form_input($data);?></div>
+
 <?php echo form_close();?>
