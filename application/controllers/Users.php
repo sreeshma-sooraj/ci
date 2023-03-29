0c087d12c642bcad7b273822f7369614b434a146
sreeshmasooraj <sreesma93@gmail.com>
2023-03-29 12:25:30 +0530

    lesson32 :  Form Validation Part 3 and using sessions

--- application/controllers/Users.php
+++ application/controllers/Users.php
@@ -3,15 +3,19 @@ class Users extends CI_Controller{
 
 public function login(){
 
-     $this->form_validation->set_rules('username','Username','trim|required|min_length[3]');
-     $this->form_validation->set_rules('password','Password','trim|required|min_length[3]');
+     $this->form_validation->set_rules('username', 'Username', 'trim|required|min_length[3]');
+     $this->form_validation->set_rules('password', 'Password', 'trim|required|min_length[3]');
+   
 
-if($this->form_validation-> run() == FALSE){
-  $data = array(
+     if ($this->form_validation->run() == FALSE) {
+     $data = array(
 
-      'errors' => validation_errors();
+      'errors' => validation_errors()
 
   );
+  //print_r($data);
+  $this->session->set_flashdata($data);
+  redirect('home');
 }
   //echo $this->input->post('username');
   //echo " this works";
--- application/views/users/login_view.php
+++ application/views/users/login_view.php
@@ -1,5 +1,10 @@
 <h1>Login Form</h1>
 <?php $attribute = array('id' => 'login_form' , 'class' => 'form_horizontal'); ?>
+
+<?php  if($this->session->flashdata('errors')): ?>
+<?php echo $this->session->flashdata('errors'); ?>
+
+<?php endif ?>    
 <?php echo form_open('users/login',$attribute);?>
 
 <div class="form-group">
