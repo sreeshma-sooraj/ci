91c984202da262ece7de8661505ca17d4845d875
sreeshmasooraj <sreesma93@gmail.com>
2023-03-30 16:56:41 +0530

    lesson47: Creating register Method and Challenge

--- application/controllers/Users.php
+++ application/controllers/Users.php
@@ -1,5 +1,13 @@
 <?php
 class Users extends CI_Controller{
+  
+  public function register(){
+
+      $data['main_view'] = 'users/register_view';
+      $this->load->view('layouts/main', $data );
+
+
+  }
 
 public function login(){
 
