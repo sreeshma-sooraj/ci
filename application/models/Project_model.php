87dcf1e7ddaec1d79441047469741a43efaf8e10
sreeshmasooraj <sreesma93@gmail.com>
2023-04-03 11:55:40 +0530

    lesson60: Displaying projects - bootstrapping part 1

--- application/models/Project_model.php
+++ application/models/Project_model.php
@@ -1,10 +1,12 @@
 <?php 
-class Project_model extends CI_Controller{
+class Project_model extends CI_Model{
 
 
 public function get_projects()
 {
 
+    $query = $this->db->get('projects');
+    return  $query->result();
     
 }
 
--- application/views/projects/index.php
+++ application/views/projects/index.php
@@ -5,3 +5,29 @@
 
 <?php endif; ?>
 </p>
+<h1>PROJECTS</h1>
+<table class="table">
+    <thead>
+<tr>
+
+<th>
+    Project Name
+</th>
+<th>
+    Project Body
+</th>
+</tr>
+
+
+</thead>
+<tbody>
+    <?php foreach($projects as $project): ?>
+        <tr>
+        <?php echo "<td>" . $project->project_name . "</td>"; ?>
+        <?php echo "<td>" . $project->project_body . "</td>"; ?>
+    </tr>
+    <?php endforeach; ?>
+</tbody>
+
+
+</table>
